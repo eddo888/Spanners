e6720c1b521a1d39c9e5bbadd15cdfafb73f0994
David Edson <dave@stibosystems.com.au>
2020-02-28 10:38:06 +1100

    fixezd

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 pwd = path.abspath(path.dirname(__file__))
-with open(path.join(pwd, 'README.md'), 'rb') as input:
+with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
     long_description = input.read()
 
 version='1.1'

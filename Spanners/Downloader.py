6a8d12c83b761bdf7dbd2ee37dd8a0a5925ca7b6
David Edson <eddo888@tpg.com.au>
2020-09-02 10:24:18 +1000

    resume

--- Spanners/Downloader.py
+++ Spanners/Downloader.py
@@ -10,14 +10,23 @@ from Baubles.Logger import Logger
 logger = Logger()
 logger.setLevel(logging.INFO)
 
+
+# todo: https://stackoverflow.com/questions/22894211/how-to-resume-file-download-in-python
+
+#_____________________________________________________________________________
 class Downloader(object):
+	'''
+	cached download manager
+	'''
 	
-	def __init__(self, cacheDir='.cache', fresh=False):
+	def __init__(self, cacheDir='.cache', fresh=False, resumable=False):
 		#urllib3.disable_warnings()
 		self.cacheDir = cacheDir
 		self.fresh = fresh
+		self.resumable = resumable
 		logger.debug('cacheDir='+self.cacheDir)
 		
+
 	def download(self, url, file=None, username=None, password=None):
 		'''
 		read from disk before downloading
@@ -49,18 +58,28 @@ class Downloader(object):
 
 		data = None
 
-		if self.fresh or not os.path.isfile(file):
+		resume_byte_pos = 0
+		if self.resumable and os.path.isfile(file):
+			stat = os.stat(file)
+			resume_byte_pos=stat.st_size #+1 ?
+			logger.info('resume_byte_pos=%d'%resume_byte_pos)
+			
+		if self.fresh or self.resumable or not os.path.isfile(file):
 			logger.info('to cache '+file)
 			if username and password:
 				auth=(username,password)
 			else:
 				auth=None
-			response = requests.get(url, verify=True, stream=True, auth=auth)
-			if response.status_code != 200:
+
+			resume_header = { 'Range': 'bytes=%d-' % resume_byte_pos}
+
+			response = requests.get(url, verify=True, headers=resume_header, stream=True, auth=auth, allow_redirects=True)
+			if not ('%d'%response.status_code).startswith('2'):
 				file = '%s=%s'%(file,response.status_code)
 				logger.warning('url=%s, code=%s'%(url, response.status_code))
-			with open(file,'wb') as output:
+			with open(file,'ab') as output:
 				response.raw.decode_content = True
+				#output.write(response.raw.read())
 				shutil.copyfileobj(response.raw, output)
 
 		else:
@@ -72,17 +91,18 @@ class Downloader(object):
 		return data.getvalue()
 
 
+#_____________________________________________________________________________
 def main():
-	downloader = Downloader(fresh=False)
-	now = arrow.now()
-	url = 'https://docs.python.org/3/library/hashlib.html' #?_=%s'%now
-	html = downloader.download(url)
-	bs = BS(html, 'html5lib')
-	print('\n%s'%bs.find('title').text)
-	#print(html)
-	#s = str(html)
-	#print('\n'.join(s.split('\\n')[:5]))
+	downloader = Downloader(fresh=False, resumable=True)
+	#now = arrow.now()
+	if len(sys.argv) == 1:
+		sys.stderr.write('usage: [<url>*]\n')
+	else:
+		for file in sys.argv[1:]:
+			html = downloader.download(sys.argv[1])
+
 
 		
+#_____________________________________________________________________________
 if __name__ == '__main__': main()
 

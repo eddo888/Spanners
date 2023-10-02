ee35b5921e1ac8c3b46ef3a0dd53bbb360986d78
David Edson <eddo888@tpg.com.au>
2023-10-02 12:27:26 +1100

    executor with retries

--- Spanners/Executor.py
+++ Spanners/Executor.py
@@ -7,76 +7,67 @@ from functools import wraps
 
 #_________________________________________________
 def getRoot(fn):
-    #print 'fn=', fn, ', name=', fn.__name__
-    while hasattr(fn,'func_closure') and fn.func_closure:
-        #print 'fn.func_closure=', fn.func_closure
-        if len(fn.func_closure) == 0:
-            break
-        fn = fn.func_closure[0].cell_contents
-    return fn
-    
+	#print 'fn=', fn, ', name=', fn.__name__
+	while hasattr(fn,'func_closure') and fn.func_closure:
+		#print 'fn.func_closure=', fn.func_closure
+		if len(fn.func_closure) == 0:
+			break
+		fn = fn.func_closure[0].cell_contents
+	return fn
+	
 #_________________________________________________
 class Executor(object):
-    '''
-    execution helpers:
-    executor = Executor()
-    '''
-    
-    def retry(self,*oargs,**okwargs):
-        '''
-        retry the enclosed function:
-        
-        @executor.retry(timeout=5)
-        def fnToRetry():   
-        '''
+	'''
+	execution helpers:
+	executor = Executor()
+	'''
+	
+	def retry(self, *oargs, **okwargs):
+		'''
+		retry the enclosed function:
+		
+		@executor.retry(timeout=5, retries=3)
+		def fnToRetry(): 
+			raise Exception('fail')
+		'''
 
-        timeout = getattr(okwargs,'timeout',5)
-        
-        def _wrapit(fn):
-            fn = getRoot(fn)
-            
-            @wraps(fn)
-            def _wrapper(*args, **kwargs):
-                # execution
-                while True:
-                    try:
-                        return fn(*args,**kwargs)
-                        break
-                    except:
-                        sys.stderr.write('%s\n'%sys.exc_info()[0])
-                        sleep(timeout)
-                        
-            return _wrapper
-                
-        if len(oargs) == 0:
-            '''
-            handle no parentheses
-            '''
-            def _actualWrapper(fn):
-                return _wrapit(fn)
-            return _actualWrapper
-        else:
-            '''
-            handle with parentheses
-            '''
-            fn = oargs[0]
-            return _wrapit(fn)
-
+		#print(oargs,okwargs)
+		
+		def _wrapit(fn):
+			fn = getRoot(fn)
+			
+			@wraps(fn)
+			def _wrapper(*args, **kwargs):
+				
+				timeout = okwargs.get('timeout', 5)
+				retries = okwargs.get('retries', 3)
+				#print(f'{timeout=} {retries=}')
+			
+				# execution
+				while retries > 0:
+					try:
+						return fn(*args,**kwargs)
+						break
+					except:
+						sys.stderr.write(f'{retries}: {sys.exc_info()[0]}\n')
+						retries -= 1
+						sleep(timeout)
+						
+			return _wrapper
+				
+		def _actualWrapper(fn): return _wrapit(fn)
+			
+		return _actualWrapper
+	
 def main():
-	global counter
-	counter = 5
-
 	executor = Executor()
 	
-	@executor.retry(timeout=1)
-	def increment():
-		global counter
-		print('counter=%d'%counter)
-		counter -= 1
-		if counter > 0:
-			raise Exception('loop')
+	@executor.retry(timeout=1) #, retries=2)
+	def awooga():
+		print('try')
+		raise Exception('fail')
 
-	increment()
+	awooga()
 	
 	return
 
